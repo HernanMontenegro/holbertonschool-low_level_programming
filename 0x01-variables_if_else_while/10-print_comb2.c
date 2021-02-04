bea9689bb193e87f5844128ab565e7a1602d5952
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-02-04 18:47:51 +0000

    auto add

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -9,24 +9,21 @@
  */
 int main(void)
 {
-	int first, second;
-
-	for (first = '0'; first <= '9'; first++)
-	{
-		for (second = '0'; second <= '9'; second++)
-		{
-			putchar(first);
-			putchar(second);
-			
-			if (first == '9' && second == '9')
-			{
-				break;
-			}
-			putchar(',');
-			putchar(' ');
-		}
-	}
-
-	putchar('\n');
-	return (0);
+int first, second;
+for (first = '0'; first <= '9'; first++)
+{
+for (second = '0'; second <= '9'; second++)
+{
+putchar(first);
+putchar(second);
+if (first == '9' && second == '9')
+{
+break;
+}
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
 }
