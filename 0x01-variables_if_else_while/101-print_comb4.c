809dcd94ce2ef4e99609980663e4391e72e72912
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-02-04 20:02:33 +0000

    auto add

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i, j, k;
+
+	for (k = '0'; i <= '7'; k++)
+	{
+		for (i = k + '1'; i <= '8'; i++)
+		{
+			for (j = i + '1'; j <= '9'; j++)
+			{
+				putchar(k);
+				putchar(i);
+				putchar(j);
+				if (k == '7' && i == '8' && j == '9')
+					break;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
