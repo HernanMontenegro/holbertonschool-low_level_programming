b7de2c57e48d9bbc67333f171dd32e41094030a9
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-02-04 18:45:07 +0000

    auto add

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,32 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int first, second;
+
+	for (first = '0'; first <= '9'; first++)
+	{
+		for (second = '0'; second <= '9'; second++)
+		{
+			putchar(first);
+			putchar(second);
+			
+			if (first == '9' && second == '9')
+			{
+				break;
+			}
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
