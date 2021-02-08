d69a236dd3a681bb88ef2c7b7d6d45981285cb1f
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-02-08 23:44:49 +0000

    auto add

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/*
+*
+*
+*
+*/
+void times_table(void)
+{
+	int i, multiplier, n, firstDigit, lastDigit;
+	
+	for (multiplier = 0; multiplier <= 9; multiplier ++)
+	{
+		for (i = 0; i <= 9; i++)
+		{
+			n = multiplier*i;
+			if (n >= 10)  
+   			{
+				firstDigit = n / 10;
+                                lastDigit = n % 10;
+
+                                _putchar(firstDigit + '0');
+                                _putchar(lastDigit + '0');	  
+   			}
+			else
+				_putchar(n + '0');
+
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
