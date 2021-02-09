f20500623f28ce364f8715d6ef1f9a39b197df67
Hernan Montenegro <77987989+HernanMontenegro@users.noreply.github.com>
2021-02-09 10:51:15 -0300

    Update 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,8 +1,12 @@
 #include "holberton.h"
 
-/*
-*
-*/
+/**
+ * add - sum two numbers
+ * @a: the first number given
+ * @b: the second number given
+ * ---------------------------------------
+ * Return: the sum of both numbers
+ */
 int add(int a, int b)
 {
 	return (a+b);
