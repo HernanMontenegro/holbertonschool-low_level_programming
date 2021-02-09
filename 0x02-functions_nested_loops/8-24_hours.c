d43706ad98b6cba75d325e48bb377d567be25e7d
Hernan Montenegro <77987989+HernanMontenegro@users.noreply.github.com>
2021-02-09 10:11:32 -0300

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,5 +1,27 @@
 #include "holberton.h"
 
+void night(int i, int j, int k, int l)
+{
+	for (i = '2'; i <= '2'; i++)
+	{
+		for (j = '0'; j <= '3'; j++)
+		{
+			for (k = '0'; k <= '5'; k++)
+			{
+				for (l = '0'; l <= '9'; l++)
+				{
+						_putchar(i);
+						_putchar(j);
+						_putchar(':');
+						_putchar(k);
+						_putchar(l);
+						_putchar('\n');
+				}
+			}
+		}
+	}
+}
+
 /**
 * jack_bauer - print every hour of the day
 * ---------------------------------
@@ -8,7 +30,7 @@
 void jack_bauer(void)
 {
 	int i, j, k, l;
-	
+
 	for (i = '0'; i <= '2'; i++)
 	{
 		for (j = '0'; j <= '9'; j++)
@@ -30,23 +52,5 @@ void jack_bauer(void)
 			}
 		}
 	}
-
-	for (i = '2'; i <= '2'; i++)
-	{
-		for (j = '0'; j <= '3'; j++)
-		{
-			for (k = '0'; k <= '5'; k++)
-			{
-				for (l = '0'; l <= '9'; l++)
-				{
-						_putchar(i);
-						_putchar(j);
-						_putchar(':');
-						_putchar(k);
-						_putchar(l);
-						_putchar('\n');
-				}
-			}
-		}
-	}
+	night(i,j,k,l);
 }
