dec62a4a1083711b7dfb57d932ea0ee927265b44
Hernan Montenegro <77987989+HernanMontenegro@users.noreply.github.com>
2021-02-09 09:44:21 -0300

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,8 +1,10 @@
 #include "holberton.h"
 
 /**
-*
-*
+* print_last_digit - return the last digit of the number given
+* @n: equal the number given
+* ---------------------------------
+* Return: the last digit of the number given
 */
 int print_last_digit(int n)
 {
