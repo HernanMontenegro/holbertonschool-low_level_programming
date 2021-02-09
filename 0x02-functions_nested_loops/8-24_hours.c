50d3d1f9bb5badaccaefddec834fdf97c5caa2eb
Hernan Montenegro <77987989+HernanMontenegro@users.noreply.github.com>
2021-02-09 10:03:24 -0300

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,38 +7,37 @@
 */
 void jack_bauer(void)
 {
-	int i,j,k,l;
+	int i, j, k ,l;
 
 	for (i = '0'; i <= '2'; i++)
 	{
 		for (j = '0'; j <= '9'; j++)
-        	{
-			if(i != '2')
+		{
+			if (i != '2')
 			{
 				for (k = '0'; k <= '5'; k++)
-                        	{
-                                	for (l = '0'; l <= '9'; l++)
-                                	{
-                                        	_putchar(i);
-                                        	_putchar(j);
-                                        	_putchar(':');
-                                        	_putchar(k);
-                                        	_putchar(l);
-                                        	_putchar('\n');
-
-                                	}
-                        	}
+				{
+					for (l = '0'; l <= '9'; l++)
+					{
+						_putchar(i);
+						_putchar(j);
+						_putchar(':');
+						_putchar(k);
+						_putchar(l);
+						_putchar('\n');
+					}
+				}
 			}
-        	}
+		}
 	}
-	
+
 	for (i = '2'; i <= '2'; i++)
         {
                 for (j = '0'; j <= '3'; j++)
                 {
                         for (k = '0'; k <= '5'; k++)
                         {
-                        	for (l = '0'; l <= '9'; l++)
+				for (l = '0'; l <= '9'; l++)
                                 {
                                                 _putchar(i);
                                                 _putchar(j);
