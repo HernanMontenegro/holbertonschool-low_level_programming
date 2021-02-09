e29bbe8ffe2e9a7fe602f549cb08700aaec20a4e
Hernan Montenegro <77987989+HernanMontenegro@users.noreply.github.com>
2021-02-09 10:48:19 -0300

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,29 +9,29 @@ void times_table(void)
 {
 	int i, multiplier, n, firstDigit, lastDigit;
 
-	for (multiplier = 0; multiplier <= 9; multiplier ++)
+	for (multiplier = 0; multiplier <= 9; multiplier++)
 	{
 		for (i = 0; i <= 9; i++)
 		{
-			n = multiplier*i;
-			if (n >= 10)  
+			n = multiplier * i;
+			if (n >= 10)
 			{
 				firstDigit = n / 10;
 				lastDigit = n % 10;
 
 				_putchar(firstDigit + '0');
-                                _putchar(lastDigit + '0');
+				_putchar(lastDigit + '0');
 				if (i == 9)
 					continue;
 				_putchar(',');
-				_putchar(' ');	  
+				_putchar(' ');
 			}
 			else
 			{
 				_putchar(n + '0');
 				if (i == 9)
 					continue;
-				if (n+multiplier >= 10)
+				if (n + multiplier >= 10)
 				{
 					_putchar(',');
 					_putchar(' ');
