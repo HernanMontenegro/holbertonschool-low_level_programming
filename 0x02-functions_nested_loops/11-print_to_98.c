6da0ddefd84da4281a63854be4deddc06cf6992c
Hernan Montenegro <77987989+HernanMontenegro@users.noreply.github.com>
2021-02-09 10:59:11 -0300

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,17 +1,20 @@
 #include "holberton.h"
 #include <stdio.h>
 
-/*
-*
+/**
+* print_to_98 - prints numberical path for reaching 98
+* @n: number to start
+* 
+* Return: the last digit of the number
 */
 void print_to_98(int n)
 {
 	if (n > 98)
 	{
-		for (n = n; n >= 98; n --)
+		for (n = n; n >= 98; n--)
 		{
 			printf("%d", n);
-			if(n == 98)
+			if (n == 98)
 			{
 				printf("\n");
 				continue;
