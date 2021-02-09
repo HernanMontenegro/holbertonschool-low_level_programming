359b3eb96d90ec4aa43ac5facf7d7db040caf731
Hernan Montenegro <77987989+HernanMontenegro@users.noreply.github.com>
2021-02-09 20:29:05 -0300

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,32 +10,27 @@ void print_times_table(int n)
 {
     int i, j;
 
-    for (; n <= n + 1; n++)
+    if (n > 15 || n < 0)
+        return;
+
+    for (i = 0; i <= n; i++)
     {
-        for (i = 0; i <= n; i++)
+        for (j = 0; j <= n; j++)
         {
-            for (j = 0; j <= n; j++)
+            if ((i * j) >= 10)
             {
-                if ((i * j) >= 10)
-                {
-                    _putchar((i * j) / 10 + '0');
-                    _putchar((i * j) % 10 + '0');
-                }
-                else if ((i * j) >= 100)
-                {
-                    _putchar((i * j) / 100 + '0');
-                    _putchar(((i * j) / 10) % 10 + '0');
-                    _putchar((i * j) / 10 + '0');
-                }
-                else
-                    _putchar((i * j) / 10 + '0');
-
-                if (j != 0 && (i * j) >= 100)
-                {
-
-                }
+                _putchar((i * j) / 10 + '0');
+                _putchar((i * j) % 10 + '0');
             }
-            _putchar('\n');
+            else if ((i * j) >= 100)
+            {
+                _putchar((i * j) / 100 + '0');
+                _putchar(((i * j) / 10) % 10 + '0');
+                _putchar((i * j) / 10 + '0');
+            }
+            else
+                _putchar((i * j) + '0');
         }
+        _putchar('\n');
     }
 }
