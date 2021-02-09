fae402a2a658dd121f48be763358913f81479dbb
Hernan Montenegro <77987989+HernanMontenegro@users.noreply.github.com>
2021-02-09 09:49:53 -0300

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,11 +8,8 @@
 */
 int print_last_digit(int n)
 {
-	int lastDigit;
-
 	if (n < 0)
 		n = -n;
-	lastDigit = n % 10;
-	_putchar(lastDigit + '0');
-	return (lastDigit);
+	_putchar((n % 10) + '0');
+	return (n % 10);
 }
