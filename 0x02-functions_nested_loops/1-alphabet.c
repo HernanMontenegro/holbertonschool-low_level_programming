2e5b1f172dde452e66627869fa92e2cc609fbed3
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-02-09 12:19:37 +0000

    auto add

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,15 @@
 #include "holberton.h"
 
-/**
+/*
 *
 *
 */
 void print_alphabet(void)
 {
-	char i;
-	for (i = 'a'; i <= 'z'; i++)
-	{
-		_putchar(i);
-	}
-	_putchar('\n');
+char i;
+for (i = 'a'; i <= 'z'; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
 }
