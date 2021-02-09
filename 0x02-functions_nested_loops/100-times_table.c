b8e26cc791dd58bfcacaea398c23a5d59d6aa962
Hernan Montenegro <77987989+HernanMontenegro@users.noreply.github.com>
2021-02-09 20:45:25 -0300

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -28,7 +28,7 @@ void print_times_table(int n)
             {
                 _putchar(((multiplication / 10) / 10) + '0');
                 _putchar(((multiplication / 10) % 10) + '0');
-                _putchar((multiplication / 10) + '0');
+                _putchar((multiplication % 10) + '0');
             }
             else
                 _putchar(multiplication + '0');
