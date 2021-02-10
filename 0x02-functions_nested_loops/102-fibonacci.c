66814d3670a6973df41cabd6568f189b57a397ff
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-02-10 16:49:59 +0000

    auto add

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -3,15 +3,15 @@
 // Driver Code
 int main()
 {
-    int f1 = 0, f2 = 1, i;
+int f1 = 1, f2 = 1, i;
 
-    for (i = 1; i < 50; i++) 
-    {
-        printf("%d, ", f2);
-        int next = f1 + f2;
-        f1 = f2;
-        f2 = next;
-    }
-    printf("\n");
-    return 0;
+for (i = 1; i <= 50; i++) 
+{
+printf("%d, ", f2);
+int next = f1 + f2;
+f1 = f2;
+f2 = next;
+}
+printf("\n");
+return 0;
 }
