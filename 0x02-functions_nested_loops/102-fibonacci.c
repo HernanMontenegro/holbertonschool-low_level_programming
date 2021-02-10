0de0200d7cf0698794816342d6ce5ab87a0c8d59
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-02-10 17:21:22 +0000

    auto add

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,14 +7,17 @@
 */
 int main(void)
 {
-unsigned int f1 = 1, f2 = 1, i;
+unsigned f1 = 1, f2 = 1, i;
 
 for (i = 0; i < 50; i++)
 {
-printf("%u, ", f2);
+printf("%u", f2);
 int next = f1 + f2;
 f1 = f2;
 f2 = next;
+if(i == 49)
+continue;
+printf(", ");
 }
 printf("\n");
 return (0);
