17e613f3d5a03a4f20ef0b46e6801c5d191fa72f
Hernan Montenegro <77987989+HernanMontenegro@users.noreply.github.com>
2021-02-09 21:03:27 -0300

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,69 +8,69 @@
 */
 void print_times_table(int n)
 {
-    int i, j;
+int i, j;
 
-    if (n > 15 || n < 0)
-        return;
+if (n > 15 || n < 0)
+return;
 
-    for (i = 0; i <= n; i++)
-    {
-        for (j = 0; j <= n; j++)
-        {
-            int multiplication = i * j;
+for (i = 0; i <= n; i++)
+{
+for (j = 0; j <= n; j++)
+{
+int multiplication = i * j;
 
-            if (multiplication >= 10 && multiplication < 100)
-            {
-                _putchar(multiplication / 10 + '0');
-                _putchar(multiplication % 10 + '0');
+if (multiplication >= 10 && multiplication < 100)
+{
+_putchar(multiplication / 10 + '0');
+_putchar(multiplication % 10 + '0');
 
-                if (j == n)
-                    continue;
-                else if((multiplication + i) >= 100)
-                {
-                    _putchar(',');
-                    _putchar(' ');
-                }
-                else
-                {
-                    _putchar(',');
-                    _putchar(' ');
-                    _putchar(' ');
-                }
-            }
-            else if (multiplication >= 100)
-            {
-                _putchar(((multiplication / 10) / 10) + '0');
-                _putchar(((multiplication / 10) % 10) + '0');
-                _putchar((multiplication % 10) + '0');
+if (j == n)
+continue;
+else if ((multiplication + i) >= 100)
+{
+_putchar(',');
+_putchar(' ');
+}
+else
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+}
+}
+else if (multiplication >= 100)
+{
+_putchar(((multiplication / 10) / 10) + '0');
+_putchar(((multiplication / 10) % 10) + '0');
+_putchar((multiplication % 10) + '0');
 
-                if (j == n)
-                    continue;
- 
-                    _putchar(',');
-                    _putchar(' ');
-            }
-            else
-            {
-                _putchar(multiplication + '0');
-                if (j == n)
-                    continue;
+if (j == n)
+continue;
 
-                else if((multiplication + i) >= 10)
-                {
-                    _putchar(',');
-                    _putchar(' ');
-                    _putchar(' ');
-                }
-                else
-                {
-                    _putchar(',');
-                    _putchar(' ');
-                    _putchar(' ');
-                    _putchar(' ');
-                }
-            }
-        }
-        _putchar('\n');
-    }
+_putchar(',');
+_putchar(' ');
+}
+else
+{
+_putchar(multiplication + '0');
+if (j == n)
+continue;
+
+else if ((multiplication + i) >= 10)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+}
+else
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(' ');
+}
+}
+}
+_putchar('\n');
+}
 }
