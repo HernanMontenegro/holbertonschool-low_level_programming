02b9ad3a993a13e7636e53a6aa1a9decee5ae090
Hernan Montenegro <77987989+HernanMontenegro@users.noreply.github.com>
2021-02-09 21:17:30 -0300

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,6 +1,58 @@
 #include "holberton.h"
 
 /**
+* print_controller - manages the number print
+* @multiplication: the number given
+* @i: the next number calculation
+* ------------------------------------
+* Return: void
+*/
+void print_controller(int multiplication, int i)
+{
+if (multiplication >= 10 && multiplication < 100)
+{
+_putchar(multiplication / 10 + '0');
+_putchar(multiplication % 10 + '0');
+}
+else if (multiplication >= 100)
+{
+_putchar(((multiplication / 10) / 10) + '0');
+_putchar(((multiplication / 10) % 10) + '0');
+_putchar((multiplication % 10) + '0');
+}
+else
+_putchar(multiplication + '0');
+}
+/**
+* spaces_controller - manages spaces and commas after printing a number value
+* @multiplication: the number given
+* @i: the next number calculation
+* ------------------------------------
+* Return: void
+*/
+void spaces_controller(int multiplication, int i)
+{
+else if((multiplication + i) >= 100)
+{
+_putchar(',');
+_putchar(' ');
+}
+else if ((multiplication + i) >= 10)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+}
+else
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(' ');
+}
+}
+
+/**
 * print_times_table - prints times tables from 0 to n while n > 15
 * @n: the number given
 * ------------------------------------
@@ -8,50 +60,21 @@
 */
 void print_times_table(int n)
 {
-    int i, j;
+int i, j;
 
-    if (n > 15 || n < 0)
-        return;
-    for (i = 0; i <= n; i++)
-    {
-        for (j = 0; j <= n; j++)
-        {
-            int multiplication = i * j;
+if (n > 15 || n < 0)
+return;
+for (i = 0; i <= n; i++)
+{
+for (j = 0; j <= n; j++)
+{
+int multiplication = i * j;
 
-            if (multiplication >= 10 && multiplication < 100)
-            {
-                _putchar(multiplication / 10 + '0');
-                _putchar(multiplication % 10 + '0');
-            }
-            else if (multiplication >= 100)
-            {
-                _putchar(((multiplication / 10) / 10) + '0');
-                _putchar(((multiplication / 10) % 10) + '0');
-                _putchar((multiplication % 10) + '0');
-            }
-            else
-                _putchar(multiplication + '0');
-            if (j == n)
-                continue;
-            else if((multiplication + i) >= 100)
-            {
-                _putchar(',');
-                _putchar(' ');
-            }
-            else if ((multiplication + i) >= 10)
-            {
-                _putchar(',');
-                _putchar(' ');
-                _putchar(' ');
-            }
-            else
-            {
-                _putchar(',');
-                _putchar(' ');
-                _putchar(' ');
-                _putchar(' ');
-            }
-        }
-        _putchar('\n');
-    }
+print_controller(multiplication, i)
+if (j == n)
+continue;
+spaces_controller(multiplication, i)
+}
+_putchar('\n');
+}
 }
