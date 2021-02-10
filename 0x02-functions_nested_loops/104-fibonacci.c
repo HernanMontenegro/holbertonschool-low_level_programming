d9ac2c6027d32a82458c4825f7a2e6e5269a5e8a
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-02-10 22:58:03 +0000

    auto add

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,14 +7,21 @@
 */
 int main(void)
 {
-    unsigned long a = 1, b = 2, res, i;
+    unsigned long a = 1, b = 2, a_half, b_half, res, res_half, i;
 
     printf("%lu, ", a);
     printf("%lu, ", b);
     for (i = 3 ; i <= 98 ; i++)
     {
         res = a + b;
-        printf("%lu", res);
+        if(res > 9999999999)
+        {
+            res_half = (a / 2) + (b / 2);
+            printf("%lu%lu", res_half, res_half);
+        }
+        else
+            printf("%lu", res);
+
         if (i != 98)
         {
                 printf(", ");
