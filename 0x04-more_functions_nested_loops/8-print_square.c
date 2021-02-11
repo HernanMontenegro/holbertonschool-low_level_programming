08e31eed0ba08bcc8f2a3ddae481cb7769ee10dc
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-02-11 16:23:18 +0000

    auto add

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,16 +8,16 @@
  */
 void print_square(int size)
 {
-    int maxSize = size, i;
+int maxSize = size, i;
 
-    if(size <= 0)
-        _putchar('\n');
-    for (size = 1; size <= maxSize; size++)
-    {
-        for (i = 1; i <= maxSize; i++)
-        {
-            _putchar('#');
-        }
-        _putchar('\n');
-    }
-}    
+if(size <= 0)
+_putchar('\n');
+for (size = 1; size <= maxSize; size++)
+{
+for (i = 1; i <= maxSize; i++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
