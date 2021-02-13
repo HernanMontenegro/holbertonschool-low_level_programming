0892b8414a49d09fc9cfc7ad1312fe99b3a60439
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-02-13 21:24:52 +0000

    auto add

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,7 +8,7 @@
  */
 void rev_string(char *s)
 {
-int i, j, temp;
+int i, j;
 
 for (i = 0; s[i] != '\0'; i++)
 {
@@ -17,8 +17,6 @@ for (i = 0; s[i] != '\0'; i++)
 i--;
 for (j = 0; j < i; j++, i--)
 {
-temp = s[j];
 s[j] = s[i];
-s[i] = temp;
 }
 }
