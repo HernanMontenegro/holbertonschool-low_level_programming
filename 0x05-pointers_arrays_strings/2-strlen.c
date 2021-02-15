47d9bfc2e401da21be4943c79f93e2cbff3b298a
Hernan Montenegro <77987989+HernanMontenegro@users.noreply.github.com>
2021-02-15 20:14:27 -0300

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,18 +1,18 @@
 #include "holberton.h"
 
 /**
- * _strlen - count the amount of chars in an array
- * @s: the direction array of chars given
- * ------------------------------------
- * Return: the count of ints
+ * _strlen - 
+ *@s: number
+ *
+ * Return: void.
  */
 int _strlen(char *s)
 {
 int i;
 
-for (i = 0; i <= s[i]; i++)
+for (i = 0; s[i] != '\0'; i++)
 {
-
 }
+
 return (i);
 }
