4d62994bbe382f7836ae50dd4b5a0ee19f7c9bca
Hernan Montenegro <77987989+HernanMontenegro@users.noreply.github.com>
2021-02-15 20:37:41 -0300

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,25 +8,22 @@
  */
 void puts_half(char *str)
 {
-int i, j, n;
+int i, j, count;
 
-for (i = 0; str[i] != '\0';i++)
+for (i = 0; str[i] != '\0'; i++)
 {
+
 }
 
-if (i % 2 != 0)
-{
-n =  ((i - 1) / 2);
-}
+if (i % 2 == 0)
+count = i / 2;
 else
-{
-n = i / 2;
-}
+count = (i - 1) / 2;
 
-for (j = n; j <= i; j++)
+i--;
+for (j = count; j <= i; j++)
 {
 _putchar(str[j]);
 }
-
 _putchar('\n');
 }
