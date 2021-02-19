7221105060950bf1a8fb3d8081540fca511c12ae
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-02-19 17:12:19 +0000

    auto add

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,15 +1,18 @@
 #include "holberton.h"
 
 /**
-* reverse_array - reverses an array given
-* @a: an array of integers
-* @n: the numbers of elements in the array
-* -----------------------------------------------------
+* reverse_array - reverse an array of integers
+* @a: the integer array
+* @n: number of elements in the array
+* ------------------------------------
 * Return: void
 */
 void reverse_array(int *a, int n)
 {
+    int i, j;
 
-
-
+    for (i = n - 1, j = 0; i >= 0; i--, j++)
+    {
+        a[j] = a[i];
+    }
 }
