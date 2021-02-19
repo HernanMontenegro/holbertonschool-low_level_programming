9d031f0cbb2787d09960a41b8e8438f01b6fbb8d
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-02-19 16:11:59 +0000

    auto add

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+* reverse_array - reverses an array given
+* @a: an array of integers
+* @n: the numbers of elements in the array
+* -----------------------------------------------------
+* Return: void
+*/
+void reverse_array(int *a, int n)
+{
+
+
+
+}
