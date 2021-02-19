9f9be3cbc8318ef3ee544aeb2ddbed1e845bed5e
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-02-19 21:19:21 +0000

    auto add

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+* rot13 - encodes a string to rot13
+* @c: the string given
+* --------------------------------------
+* Return: new string
+*/
+char *rot13(char *c)
+{
+	int i, j;
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz";
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		for (j = 0; alphabet[j] != '\0'; j++)
+		{
+			if (c[i] == alphabet[j])
+				c[i] = alphabet[j + 13];
+		}
+	}
+
+	return (c);
+}
