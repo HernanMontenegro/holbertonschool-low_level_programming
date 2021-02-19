6a8fffd595654589d9fa75fd10d215ef83280816
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-02-19 16:16:11 +0000

    auto add

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,63 +1,32 @@
 #include "holberton.h"
 
-
-
 /**
-
 * _strncpy - copy two strings
-
 * @dest: the string destination
-
 * @src: the string source
-
 * @n: the bytes to copy
-
 * ------------------------------------
-
 * Return: void
-
 */
-
 char *_strncpy(char *dest, char *src, int n)
-
 {
-
 int i, j;
 
-
-
 for (i = 0; src[i] != '\0'; i++)
-
 {
 
-
-
 }
-
 for (j = 0; j < n; j++)
-
 {
-
 if (j < i)
-
 {
-
 dest[j] = src[j];
-
 }
-
 else
-
 {
-
 dest[j] = '\0';
-
 }
-
 }
-
-
 
 return (dest);
-
 }
