07e3db5009d7197bfadc6617710a3b6865272dcb
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-02-19 16:58:38 +0000

    auto add

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,21 +9,28 @@
 */
 int _strcmp(char *s1, char *s2)
 {
-	int i, s1_length, s2_length, res = 0;
-	for (s1_length = 0; s1[s1_length] != '\0'; s1_length++)
-	{
-		
-	}
+int i, s1_length, s2_length, maximum, res = 0;
 
+for (s1_length = 0; s1[s1_length] != '\0'; s1_length++)
+{
+}
+for (s2_length = 0; s1[s2_length] != '\0'; s2_length++)
+{
+}
 
-	for (i = 0; s1[i] != '\0'; i++)
-	{
-		if (s1[i] != s2[i])
-		{
-			res = s1[i] - s2[i];
-			break;
-		}
-	}
+if (s1_length >= s2_length)
+maximum = s1_length;
+else
+maximum = s2_length
 
-	return (res);
+for (i = 0; i < maximum; i++)
+{
+if (s1[i] != s2[i])
+{
+res = s1[i] - s2[i];
+break;
+}
+}
+
+return (res);
 }
