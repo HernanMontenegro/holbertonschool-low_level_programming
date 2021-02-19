0cbf9d9c01efb24787ca0ac7e2e14f2bb177c981
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-02-19 17:41:01 +0000

    auto add

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,10 +9,12 @@
 */
 void reverse_array(int *a, int n)
 {
-    int i, j;
+	int i, j, temp;
 
-    for (i = n, j = 0; i >= 0; i--, j++)
-    {
-        a[j] = a[i];
-    }
+    	for (i = n - 1, j = 0; j != (n - 1) / 2; i--, j++)
+    	{
+		temp =  a[j];
+        	a[j] = a[i];
+		a[i] = temp;
+    	}
 }
