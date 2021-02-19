d236fdf28a4c40b42c827033284e98790dfaaf07
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-02-19 17:01:54 +0000

    auto add

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,14 +14,14 @@ int i, s1_length, s2_length, maximum, res = 0;
 for (s1_length = 0; s1[s1_length] != '\0'; s1_length++)
 {
 }
-for (s2_length = 0; s1[s2_length] != '\0'; s2_length++)
+for (s2_length = 0; s2[s2_length] != '\0'; s2_length++)
 {
 }
 
 if (s1_length >= s2_length)
 maximum = s1_length;
 else
-maximum = s2_length
+maximum = s2_length;
 
 for (i = 0; i < maximum; i++)
 {
