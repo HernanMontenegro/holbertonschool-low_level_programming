156a89f8b9b5ebf6179c5288d4eb804168974313
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-02-23 19:08:33 +0000

    auto add

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -12,6 +12,9 @@ char *_strstr(char *haystack, char *needle)
 	int i, j;
 	char *p = 0;
 
+	if (haystack[0] == '\0')
+		return (p);
+
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
 		int hasEncountered = 0;
