dce1716e89a5df831faa37adf82be71ed43f8a2a
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-02-23 23:02:17 +0000

    auto add

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+* set_string - change string destination to another
+* @s: the string source
+* @to: the string destination
+* --------------------------------------------
+* Return: void
+*/
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
