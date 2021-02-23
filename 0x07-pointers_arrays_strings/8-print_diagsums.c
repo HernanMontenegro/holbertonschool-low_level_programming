aa4072ac942b62cf3addcd5f3619206af76dc4c3
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-02-23 22:46:46 +0000

    auto add

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+*
+*
+*/
+void print_diagsums(int *a, int size)
+{
+	int i, sum1 = 0, sum2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		sum1 += *(a + (i * size + i));
+	}
+	for (i = 1; i <= size; i++)
+	{
+		sum2 += *(a + (i * size - i));
+	}
+
+	printf("%d, %d\n", sum1, sum2);
+}
