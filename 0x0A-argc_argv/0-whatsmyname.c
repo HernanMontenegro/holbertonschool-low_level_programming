eaf3e4569d98c0bce76a6a2c54043f0c007af380
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-02-26 16:38:27 +0000

    auto add

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - the main fuction of the program
+* @argc: the array index for each command flag
+* @argv: the array of commands flags
+* ----------------------------
+* Return: void
+*/
+void main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+}
