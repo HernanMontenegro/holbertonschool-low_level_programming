11ea99ed207129800b3f6de1c2ff3f9ff4d59d8c
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-03-02 16:03:07 +0000

    auto add

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,9 +2,11 @@
 #include <stdlib.h>
 
 /**
-*
-*
-*
+* create_array - creates an array with an specific char
+* @size: the size of the array
+* @c: the char for each position
+* ------------------------------------------------------
+* Return: a char array
 */
 char *create_array(unsigned int size, char c)
 {
@@ -16,5 +18,5 @@ char *create_array(unsigned int size, char c)
 		str[i] = c;
 	}
 
-	return(str);
+	return (str);
 }
