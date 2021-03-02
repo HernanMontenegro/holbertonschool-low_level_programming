00c1725f475b6c4cac1c7c641bcd9a3f26af7e21
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-03-02 20:08:00 +0000

    auto add

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -9,11 +9,11 @@
 */
 int **alloc_grid(int width, int height)
 {
-	if (width < 0 || height < 0)
-		return (NULL);
-
 	int i, j;
 	int **grid;
+
+        if (width < 0 || height < 0)
+                return (NULL);
 
 	grid = malloc(sizeof(int *) * height);
 	if (!grid)
