d9fe983b46726e9886e03cbe65ad4e36cc948faa
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-03-02 18:37:18 +0000

    auto add

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -23,16 +23,21 @@ char *str_concat(char *s1, char *s2)
 	if (!fallout)
 		return (NULL);
 
+	i = 0;
+	j = 0;
+
 	if (s1)
 	{
-		for (i = 0; s1[i] != '\0'; i++)
+		for (; s1[i] != '\0'; i++)
 			fallout[i] = s1[i];
 	}
 	if (s2)
 	{
-		for (j = 0; s2[j] != '\0'; j++)
+		for (; s2[j] != '\0'; j++)
 			fallout[i + j] = s2[j];
 	}
+	if (!s1 && !s2)
+		fallout = "";
 
 	return (fallout);
 }
