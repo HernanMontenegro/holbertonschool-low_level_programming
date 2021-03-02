a2d05115fef6fd0d1f15f27e307cfa473bdf6987
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-03-02 16:06:50 +0000

    auto add

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,8 +10,12 @@
 */
 char *create_array(unsigned int size, char c)
 {
+	if (size = 0)
+		return (NULL);
+
 	char *str;
 	unsigned int i;
+
 	str = malloc(sizeof(char) * size);
 
 	for (i = 0; i < (sizeof(char) * size); i++)
