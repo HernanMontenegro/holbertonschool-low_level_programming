c4ae894462a6cb756a2d711847ba1a83e59da13a
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-03-02 19:48:07 +0000

    auto add

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,7 +1,11 @@
 #include <stdlib.h>
 
 /**
-*
+* alloc_grid - alloc a grid
+* @width: the grid width
+* @height: the grid height
+* ------------------------------------
+* Return: void
 */
 int **alloc_grid(int width, int height)
 {
@@ -20,7 +24,7 @@ int **alloc_grid(int width, int height)
 			grid[i] = malloc(sizeof(int) * width);
 			if (!grid[i])
 			{
-				for (; i>= 0; i--)
+				for (; i >= 0; i--)
 				{
 					free(grid[i]);
 				}
