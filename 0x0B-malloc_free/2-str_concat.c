4dc7ad4f3071b09947fd2c9a39f0e08091de6f39
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-03-02 17:28:19 +0000

    auto add

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+
+/**
+*
+*/
+char *str_concat(char *s1, char *s2)
+{
+	char *fallout;
+	int i, j;
+
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+	}
+	for (j = 0; s2[j] != '\0'; j++)
+
+	fallout = malloc((sizeof(char) * i + 1) + (sizeof(char) * j + 1));
+
+	for (i = 0; s1[i] != '\0'; i++)
+		fallout[i] = s1[i];
+	for (j = 0; s2[j] != '\0'; j++)
+		fallout[i + j] = s2[j];
+	fallout[i + j] = '\0';
+
+	return(fallout);
+}
