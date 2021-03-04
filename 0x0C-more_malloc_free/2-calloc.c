7f63a13fa634dc57d08f836d9b4ddb17ce7e7a1d
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-03-04 18:54:12 +0000

    auto add

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,7 +9,8 @@
 */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *p;
+	int i;
+	char *p;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
@@ -17,6 +18,9 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	p = malloc(size * nmemb);
 	if (!p)
 		return (NULL);
+
+	for (i = 0; p[i]; i++)
+		p[i] = 0;
 
 	return (p);
 }
