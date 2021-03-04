cb10a71ca50645ca545474d531aed2ee1954d1bc
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-03-04 13:11:19 +0000

    auto add

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+
+/**
+* malloc_checked - allocate memory using malloc
+* @b: memory size
+* ---------------------------------
+* Return: pointer to that memory adress
+*/
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+	if (!p)
+	{
+		b = 98;
+		exit (b);
+	}
+
+	return (p);
+}
