ab8409599db3ba7adf66b8eca2601dded699033a
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-03-04 22:06:48 +0000

    auto add

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+
+/**
+*
+*
+*/
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	unsigned int i, target;
+	char *p, *tempPTR;
+
+	if (!ptr)
+	{
+		p = malloc(new_size);
+		if (!p)
+			return (NULL);
+		return (p);
+	}
+	else if (new_size == old_size)
+		return (ptr);
+	else if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	if (new_size > old_size)
+		target = old_size;
+	else
+		target = new_size;
+
+	tempPTR = ptr;
+
+	p = malloc(new_size);
+	if (!p)
+		return (NULL);
+
+	for (i = 0; i < target; i++)
+		p[i] = tempPTR[i];
+
+	free(ptr);
+	return (p);
+}
