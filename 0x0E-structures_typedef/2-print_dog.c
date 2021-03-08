070926296a21fe8f85110d0c491da7061fb303f0
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-03-08 16:49:01 +0000

    auto add

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -18,11 +18,9 @@ void print_dog(struct dog *d)
 		printf("%s\n", (*d).name);
 	else
 		printf("(nil)\n");
-	printf("Age: ");
-	if ((*d).age != 0)
-		printf("%f\n", (*d).age);
-	else
-		printf("(nil)\n");
+
+	printf("Age: %f\n", (*d).age);
+
 	printf("Owner: ");
 	if ((*d).owner != NULL)
 		printf("%s\n", (*d).owner);
