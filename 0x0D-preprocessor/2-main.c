ca77425db6218cbe2aefc33ae79cf71daae411b2
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-03-08 13:14:06 +0000

    2-main

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+* -------------------------
+* Return: 0 if everything is fine
+*/
+int main(void)
+{
+
+	printf("%s\n", __FILE__);
+	return (0);
+}
