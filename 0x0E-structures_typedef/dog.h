9e3ecb25838d51f4f25504a1bae979c96eae6e1e
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-03-08 14:30:12 +0000

    auto add

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,7 @@
-void init_dog(struct dog *d, char *name, float age, char *owner);
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
