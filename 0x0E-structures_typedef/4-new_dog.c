b0c93741a7e950b1f19e78bf134b142c38248193
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-03-08 17:01:40 +0000

    auto add

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -8,11 +8,27 @@
 */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	int i, j;
 	dog_t *d;
+	char *copy_name, *copy_owner;
 
 	d = malloc(sizeof(dog_t));
 	if (!d)
 		return (NULL);
+
+	copy_name = malloc(sizeof(name));
+	if (!copy_name)
+		return (NULL);
+	for (i = 0; name[i]; i++)
+		copy_name[i] = name[i];
+	copy_name[i] = '\0';
+
+	copy_owner = malloc(sizeof(owner));
+	if (!copy_owner)
+		return (NULL);
+	for (j = 0; owner[j]; j++)
+		copy_owner[j] = owner[j];
+	copy_owner[j] = '\0';
 
 	(*d).name = name;
 	(*d).age = age;
