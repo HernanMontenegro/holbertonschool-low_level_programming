82a01db7338bb2d22d64d493c0c90a1676f2b7d1
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-03-10 16:54:14 +0000

    auto add

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,10 +12,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0)
-		return (-1);
-
-	if (!cmp)
+	if (!cmp || !array || size <= 0)
 		return (-1);
 
 	for (i = 0; i < size; i++)
