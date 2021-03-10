510e61d8a840b43f28536a08a0151c510c27fa05
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-03-10 13:21:59 +0000

    auto add

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+* print_name - prints a name and calls the given function
+* @name: string to print
+* @f: the pointer to a print function
+* ---------------------------------------
+* Return: void
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
