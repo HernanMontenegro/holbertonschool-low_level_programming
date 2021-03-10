3636fc9779cfced2947879c8f5d1b86fe6454e39
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-03-10 13:47:49 +0000

    auto add

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+
+/**
+* array_iterator - prints an array of ints given a function
+* @array: the array to print
+* @size: the array length
+* @action: the function to call
+* ------------------------------------
+* Return: void
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
