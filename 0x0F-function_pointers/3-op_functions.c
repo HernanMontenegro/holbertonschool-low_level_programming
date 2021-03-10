f413dafadbdcb1efcdbc7bd45e7f573d5cf39b38
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-03-10 18:30:17 +0000

    auto add

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -4,48 +4,58 @@
 * op_add - operation addition
 * @a: first integer
 * @b: second integer
+* -------------------------
+* Return: operation
 */
 int op_add(int a, int b)
 {
-	return(a + b);
+	return (a + b);
 }
 
 /**
 * op_sub - operation substrain
 * @a: first integer
 * @b: second integer
+* -------------------------
+* Return: operation
 */
 int op_sub(int a, int b)
 {
-	return(a - b);
+	return (a - b);
 }
 
 /**
 * op_mul - operation multiply
 * @a: first integer
 * @b: second integer
+* -------------------------
+* Return: operation
 */
 int op_mul(int a, int b)
 {
-	return(a * b);
+	return (a * b);
 }
 
 /**
 * op_div - operation division
 * @a: first integer
 * @b: second integer
+* -------------------------
+* Return: operation
 */
 int op_div(int a, int b)
 {
-	return(a / b);
+	return (a / b);
 }
 
 /**
 * op_mod - operation module
 * @a: first integer
 * @b: second integer
+* -------------------------
+* Return: operation
 */
 int op_mod(int a, int b)
 {
-	return(a % b);
+	return (a % b);
 }
