e6b0c2708b41fc74d0b4aaf93a4244108106ca8d
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-03-10 14:05:56 +0000

    auto add

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+
+/**
+* int_index - return the array index with the value searched
+* @array: the integer array given
+* @size: the array size
+* @cmp: the function to call
+* ------------------------------------------
+* Return: the index with the number found, -1 if fails
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size < 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
+
+	return (-1);
+}
