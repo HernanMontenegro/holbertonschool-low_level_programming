e3e7cb0876822acee3ee515e2ee0c4a8751f0e81
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-03-11 19:01:29 +0000

    1-completed-vamos-pi-v

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+
+/**
+* print_numbers - print numbers you give!! <3
+* @separator: what is separating the numbers
+* @n: the arguments length
+* -----------------------------------
+* Retrun: void
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+
+	va_list args;
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(args, int));
+
+		if (separator && (i + 1 != n))
+			printf("%s", separator);
+
+		if (i + 1 == n)
+			printf("\n");
+	}
+}
