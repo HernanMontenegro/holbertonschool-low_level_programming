e963c8eedd3166b7f61b61f46a7951d0e9d05d16
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-03-11 18:40:35 +0000

    va-list-satanic-stuff

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+
+/**
+* sum_them_all - sum all the integers you give
+* @n: integer length
+* @...: the integers
+* ---------------------------------
+* Return: value if it works, 0 if not
+*/
+int sum_them_all(const unsigned int n, ...)
+{
+	int value;
+	unsigned int i;
+	va_list args;
+	va_start(args, n);
+
+	if (n == 0)
+		return (0);
+
+	value = 0;
+	for (i = 0; i < n; i++)
+		value += va_arg(args,int);
+
+	va_end(args);
+
+	return (value);
+}
