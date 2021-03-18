9ee8159344e2d38ca009ce7665c18a29ed5a8f75
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-03-18 20:46:53 +0000

    uwu

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -8,18 +8,17 @@
 */
 void free_list(list_t *head)
 {
-	list_t *current = head;
 	list_t *next;
 
 	if (!head)
 		return;
 
-   	while (current)
+   	while (head)
    	{
-		next = current->next;
-		if (current->str)
-			free(current->str);
-		free(current);
-		current = next;
+		next = head->next;
+		if (head->str)
+			free(head->str);
+		free(head);
+		head = next;
 	}
 }
