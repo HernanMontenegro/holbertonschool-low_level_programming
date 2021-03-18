c3429c690b4e6348182d786f888d601c1c2d9162
Hernan Montenegro <77987989+HernanMontenegro@users.noreply.github.com>
2021-03-18 12:49:03 -0300

    Updating lists.h with libraries

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -5,6 +5,13 @@
 /* Holberton given function */
 int _putchar(char c);
 
+/* Included libraries */
+
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
 /*============================*/
 /**
  * struct list_s - singly linked list
