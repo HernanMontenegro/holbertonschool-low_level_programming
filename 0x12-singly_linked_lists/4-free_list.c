c7bd7047fe00700c5a67a575e17652c1b4bce370
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-03-18 20:50:34 +0000

    this works

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -13,12 +13,14 @@ void free_list(list_t *head)
 	if (!head)
 		return;
 
-   	while (head)
-   	{
+	while (head)
+	{
 		next = head->next;
 		if (head->str)
 			free(head->str);
 		free(head);
 		head = next;
 	}
+
+	head = NULL;
 }
