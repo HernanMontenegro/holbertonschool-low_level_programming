45d60e2cfbe767d608bb9aa940ae7dbe48330117
Hernan Montenegro <77987989+HernanMontenegro@users.noreply.github.com>
2021-03-18 12:46:41 -0300

    Preparing first exercise

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+* print_list - prints all elements of a list
+* @h: recieves a node
+* ---------------------
+* Return: the number of nodes
+*/
+size_t print_list(const list_t *h)
+{
+
+
+/* Return something */
+}
