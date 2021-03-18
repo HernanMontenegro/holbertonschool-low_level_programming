26af756706cc2fa659a51a42d68785f7cf69d008
Hernan Montenegro <77987989+HernanMontenegro@users.noreply.github.com>
2021-03-18 12:57:27 -0300

    Creation of exercise 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+* list_len - calculates the length of a list
+* @h: list element
+* --------------------------
+* Retuen: the length of the list
+*/
+size_t list_len(const list_t *h)
+{
+
+
+/* Return something */
+}
