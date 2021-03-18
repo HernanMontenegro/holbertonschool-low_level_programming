89e4216aae0b758d3be612be4694d5cc4656db83
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-03-18 20:21:10 +0000

    trying to solve 2 problem

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,6 +1,21 @@
 #include "lists.h"
 
+/**
+* _strlen - return the length of the string
+* @s: string
+* ------------------------
+* Return: void.
+*/
+int _strlen(const char *s)
+{
+	int i;
 
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+
+	return (i);
+}
 
 /**
 * add_node - adds a new node
@@ -11,9 +26,34 @@
 */
 list_t *add_node(list_t **head, const char *str)
 {
-	/* only happen one time */
+	list_t *new_obj, *previous = NULL;
+
+	/* this just will happen 1 time */
 	if (!*head)
-		first_position(*head);
+	{
+		new_obj = malloc(sizeof(list_t));
+		if (!new_obj)
+			return (NULL);
+		new_obj->len = _strlen(str);
+		new_obj->str = strdup(str);
+		new_obj->next = NULL;
+		*head = new_obj;
 
+		return (*head);
+	}
 
+	previous = (*head)->next;
+
+	new_obj = malloc(sizeof(list_t));
+		if (!new_obj)
+			return (NULL);
+	new_obj->str = strdup(str);
+	new_obj->len = _strlen(str);
+	new_obj->next = previous;
+
+	(*head)->str = new_obj->str;
+	(*head)->len = new_obj->len;
+	(*head)->next = new_obj;
+
+	return (new_obj);
 }
