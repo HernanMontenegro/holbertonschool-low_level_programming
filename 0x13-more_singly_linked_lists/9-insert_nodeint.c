1ae1f053b8425e145614872a9e9f4fbbd45f12df
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-03-22 21:45:27 +0000

    adding 10git add .!

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,17 +9,17 @@
 */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-        unsigned int count = 0;
-        listint_t *pos;
+	unsigned int count = 0;
+	listint_t *pos;
 
-        pos = head;
-        while (pos && count != index)
-        {
-                pos = pos->next;
-                count++;
-        }
+	pos = head;
+	while (pos && count != index)
+	{
+		pos = pos->next;
+		count++;
+	}
 
-        return (pos);
+	return (pos);
 }
 
 /**
