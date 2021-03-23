efb59b043c44259c4e521e251fb20e1aa58e5b08
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-03-23 13:40:03 +0000

    fixing 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,53 +1,29 @@
 #include "lists.h"
+#include "7-get_nodeint.c"
 
-/**
-* get_nodeint_at_index - search by index
-* @head: the head node
-* @index: the index to search
-* --------------------------------------------
-* Return: the node index
-*/
-listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
-{
-	unsigned int count = 0;
-	listint_t *pos;
-
-	pos = head;
-	while (pos && count != index)
-	{
-		pos = pos->next;
-		count++;
-	}
-
-	return (pos);
-}
-
-/**
-* insert_node_at_index - add a new node in specific index
-* @head: the head node
-* @idx: index to insert
-* @n: n value of the new node
-* -----------------------------------
-* Return: a new node in that index
-*/
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new_obj;
-	listint_t *previous;
-	listint_t *target;
+	listint_t *new_obj = NULL;
+	listint_t *previous = NULL;
+	listint_t *target = NULL;
 
-	previous = get_nodeint_at_index(*head, idx - 1);
+	if (idx != 0)
+		previous = get_nodeint_at_index(*head, idx - 1);
+
 	target = get_nodeint_at_index(*head, idx);
 
 	/* Create the new node  */
 	new_obj = malloc(sizeof(listint_t));
-	if (!new_obj || !target || !previous)
+	if (!new_obj || !target)
 		return (NULL);
 	new_obj->n = n;
 	new_obj->next = target;
 
 	/* Set the previous node next the new node  */
-	previous->next = new_obj;
+	if (idx != 0)
+		previous->next = new_obj;
+	else
+		*head = new_obj;
 
 	return (new_obj);
 }
