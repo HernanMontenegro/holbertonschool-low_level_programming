479797422c52424c0e091ae4f60154f5454734c4
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-03-23 13:59:23 +0000

    now?

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -2,6 +2,29 @@
 #include "7-get_nodeint.c"
 
 /**
+* listint_len - counts the list length
+* @h: a node given
+* ------------------------
+* Return: the length of a list
+*/
+unsigned int listint_count(const listint_t *h)
+{
+	unsigned int count = 0;
+	listint_t *current_h = (listint_t *) h;
+
+	if (!h)
+		return (count);
+
+	while (current_h)
+	{
+		count++;
+		current_h = current_h->next;
+	}
+
+	return (count);
+}
+
+/**
 * insert_nodeint_at_index - insert a new node at index
 * @head: the head node
 * @idx: the index to insert
@@ -22,8 +45,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	/* Create the new node  */
 	new_obj = malloc(sizeof(listint_t));
-	if (!new_obj || !*head)
+	if (!new_obj)
 		return (NULL);
+	else if (!*head || listint_count(*head) < idx)
+	{
+		free(new_obj);
+		return (NULL);
+	}
 	new_obj->n = n;
 	new_obj->next = target;
 
