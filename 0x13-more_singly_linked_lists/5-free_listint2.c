548b09482211ada0b5d2d34dd7104e0df839b1f0
Hernan Montenegro <77987989+HernanMontenegro@users.noreply.github.com>
2021-03-23 06:25:20 -0700

    Update 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,13 +10,13 @@ void free_listint2(listint_t **head)
 {
 	listint_t *next;
 
-	if (!head)
+	if (!*head)
 		return;
 
 	while (*head)
 	{
 		next = (*head)->next;
-		free(*next);
+		free(*head);
 		*head = next;
 	}
 	head = NULL;
