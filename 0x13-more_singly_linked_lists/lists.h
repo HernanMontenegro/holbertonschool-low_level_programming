cd66787cfad981b1b827f7ec9c40ee7a73b20b74
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-03-23 11:43:34 +0000

    memento mori

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -21,8 +21,8 @@ int _putchar(char c);
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 /* ============================ */
 
