7fc207a6123ca5a454480782ee8636d48131898d
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-03-25 17:33:55 +0000

    14 start

---
+++ 0x14-bit_manipulation/my_lib.c
@@ -0,0 +1,55 @@
+#include "holberton.h"
+
+/**
+* isBinary - Checks if the string is a binary
+* @b: the binary string
+* ----------------------------------
+* Return: 1 if is a valid string, 0 if not
+*/
+int isBinary(const char *b)
+{
+	int i;
+
+	if (!b)
+		return (0);
+
+	for (i = 0; b[i]; i++)
+	{
+		if (!((b[i] >= '0') && (b[i] <='1')))
+			return (0);
+	}
+
+	return (1);
+}
+
+/**
+ * _atoi - convert a string to it's integer value
+ * @s: the string given
+ * ------------------------------------
+ * Return: void
+ */
+int _atoi(char *s)
+{
+	int res = 0, sign = 1, i = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		int firstDigit_encounter = 0;
+
+		if(s[i] == ';')
+		break;
+		if(s[i] >= '0' && s[i] <= '9')
+		{
+			res = res * 10 + s[i] - '0';
+			firstDigit_encounter = 1;
+		}
+
+		if(firstDigit_encounter == 1)
+		{
+			if(s[i - 1] == '-')
+				sign = -1;
+		}
+	}
+
+	return sign * res;
+}
