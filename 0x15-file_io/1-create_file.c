d10c9ea87df8146acc53ffae29e274eb7ea6d5b9
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-03-29 19:55:50 +0000

    asd

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	for (i = 0; text_content[i]; i++)
+	for (i = 0; text_content && text_content[i]; i++)
 	{
 		if (write(fd, &text_content[i], 1) == -1)
 		{
