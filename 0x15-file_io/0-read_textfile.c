cdd541e1729ebaca2c53c1ac6f24002f2980f1d6
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-03-29 17:58:57 +0000

    0 finish

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,6 +9,28 @@
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	int fp = 0;
+	ssize_t count = 0, found = 0;
+	char *store = malloc(sizeof(char) * letters);
 
+	if (!filename || !store)
+		return (0);
 
+	/* Get file descriptor  */
+	fp = open(filename, O_RDONLY);
+	if (fp == -1)
+		return (0);
+	/* Check if there exists enough letters in the file  */
+	found = read(fp, store, letters);
+	if (found != -1)
+	{
+		count = write(STDOUT_FILENO, store, letters);
+		if (count == -1)
+			return (0);
+	}
+
+	close(fp);
+	free(store);
+
+	return (count);
 }
--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -2,7 +2,11 @@
 #define HOLBERTOWN_H
 
 /* Libraries  */
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 /* Prototypes  */
 ssize_t read_textfile(const char *filename, size_t letters);
