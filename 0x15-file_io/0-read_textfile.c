ab0cd589f0455465225eba3a77990b942679ca91
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-03-29 14:44:55 +0000

    starting 0x15

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+* read_textfile - reads a text file and prints it to the POSIX standard output
+* @filename: the file to t
+*
+*
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+
+
+}
---
+++ 0x15-file_io/holberton.h
@@ -0,0 +1,12 @@
+#ifndef HOLBERTOWN_H
+#define HOLBERTOWN_H
+
+/* Libraries  */
+#include <stdlib.h>
+
+/* Prototypes  */
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
+#endif /* HOLBERTOWN_H  */
