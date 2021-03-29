c4a98e92e6e815ec63c213c4532aaed1865e40b3
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-03-29 23:11:01 +0000

    PUEDE SER PA?

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,77 @@
+#include "holberton.h"
+#include <stdio.h>
+
+void FileOpener(char **, char **, int *, int *);
+
+/**
+* main - entry point
+* @ac: the count of arguments
+* @av: argument array
+*
+* Return: 0 if success, 1 if not
+*/
+int main(__attribute__((unused))int ac, char **av)
+{
+	int fp = 0, to_fp = 0, fp_bytes = 1024, to_fp_bytes = 0;
+	char *filename, *destFileName;
+	char store[1024];
+
+	if (!av[1] || ac > 3)
+		return (1);
+
+	filename = av[1];
+	destFileName = av[2];
+
+	FileOpener(&filename, &destFileName, &fp, &to_fp);
+
+	while (fp_bytes == 1024)
+	{
+		fp_bytes = read(fp, store, 1024);
+		if (fp_bytes == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
+			exit(98);
+		}
+		to_fp_bytes = write(to_fp, store, fp_bytes);
+		if (to_fp_bytes < fp_bytes)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", destFileName);
+			exit(99);
+		}
+	}
+	if (close(fp) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fp);
+		exit(100);
+	}
+	if (close(to_fp) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", to_fp);
+		exit(100);
+	}
+	return (0);
+}
+
+/**
+* FileOpener - open the files
+* @filename: first file name
+* @destFileName: target file name
+* @fp: open status check
+* @to_fp: open status target file check
+*/
+void FileOpener(char **filename, char **destFileName, int *fp, int *to_fp)
+{
+	*fp = open(*filename, O_RDONLY);
+	if (*fp == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", *filename);
+		exit(98);
+	}
+	*to_fp = open(*destFileName, O_CREAT | O_RDWR | O_TRUNC, 0664);
+	if (*to_fp == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", *destFileName);
+		close(*fp);
+		exit(99);
+	}
+}
--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -8,6 +8,9 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+/* External  */
+int _putchar(char c);
+
 /* Prototypes  */
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
