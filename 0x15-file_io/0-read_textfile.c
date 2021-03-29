1632f7a88a2e7ff9690c4133c9588347a396f4de
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-03-29 19:13:30 +0000

    0 fix?

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
 * read_textfile - reads a text file and prints it to the POSIX standard output
@@ -11,15 +12,22 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fp = 0;
 	ssize_t count = 0, found = 0;
-	char *store = malloc(sizeof(char) * letters);
+	char *store;
 
-	if (!filename || !store)
+	if (!filename)
+		return (0);
+
+	store = malloc(sizeof(char) * letters);
+	if (!store)
 		return (0);
 
 	/* Get file descriptor  */
 	fp = open(filename, O_RDONLY);
 	if (fp == -1)
+	{
+		free(store);
 		return (0);
+	}
 	/* Check if there exists enough letters in the file  */
 	found = read(fp, store, letters);
 	if (found != -1)
--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,16 +10,16 @@ int create_file(const char *filename, char *text_content)
 	int fd = 0, i;
 
 	if (!filename)
-		return (0);
+		return (-1);
 
 	fd = open(filename, O_CREAT | O_RDWR, 0600);
 	if (fd == -1)
-		return (0);
+		return (-1);
 
 	for (i = 0; text_content[i]; i++)
 	{
 		if (_putchar(text_content[i]) == -1)
-			return (0);
+			return (-1);
 	}
 	close(fd);
 	return (1);
