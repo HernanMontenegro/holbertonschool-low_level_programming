b018a92d3192d8b17aacdd853a4dfe78bde56e0e
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-04-30 20:11:00 +0000

    fixing 7 border cases I found... yes... I FOUND :D

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+#include "5-get_dnodeint.c"
+
+/**
+ * insert_dnodeint_at_index - adds a new node in an index
+ * @h: the head node
+ * @idx: the index to insert
+ * @n: the int of the new node
+ * ----------------------------------------------
+ * Return: the new node at certain index
+*/
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new = NULL, *target_indx = NULL, *get_next = NULL;
+
+	target_indx = get_dnodeint_at_index(*h, idx);
+	if (idx > 0)
+		get_next = get_dnodeint_at_index(*h, idx - 1);
+
+	if (!h || !(*h) || !target_indx)
+		return (NULL);
+
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
+		return (NULL);
+	/* Create new node */
+	new->n = n;
+	new->next = target_indx;
+	new->prev = NULL;
+	target_indx->prev = new;
+
+	if (idx > 0)
+	{
+		new->prev = get_next;
+		get_next->next = new;
+	}
+	else
+		*h = new;
+
+	return (new);
+}
---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,7 @@
+#include "lists.h"
+#include "5-get_dnodeint.c"
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+
+}
