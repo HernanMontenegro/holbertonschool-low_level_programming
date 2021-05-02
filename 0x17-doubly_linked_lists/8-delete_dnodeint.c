a39fcfc5249a4bd512ef2c978b9ac2cb5e964629
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-05-02 19:08:19 -0300

    8 fixing border case

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -13,13 +13,20 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *target = NULL, *t_prev = NULL, *t_next = NULL;
 
-	if ((index > dlistint_len(*head) - 1) || (!head || !(*head)))
+	if (!head || !(*head))
+		return (-1);
+
+	if ((index > dlistint_len(*head) - 1))
 		return (-1);
 
 	if (index > 0)
 		t_prev = get_dnodeint_at_index(*head, index - 1);
 	target = get_dnodeint_at_index(*head, index);
-	t_next = get_dnodeint_at_index(*head, index + 1);
+	if (index != dlistint_len(*head) - 1)
+	{
+		t_next = get_dnodeint_at_index(*head, index + 1);
+		t_next->prev = NULL;
+	}
 
 	if (index > 0)
 	{
