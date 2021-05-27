5a4567ea6863b5027ee008a8eb8a52378ad9a863
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-05-27 16:50:47 -0300

    ssaas

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,11 +10,14 @@
 */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int indx = key_index((const unsigned char *)key, ht->size);
+	unsigned long int indx = 0;
 	hash_node_t *new_node = NULL;
 
 	if (!ht || !key || strcmp(key, "") == 0)
 		return (0);
+
+	printf("1====================================\n");
+	indx = key_index((const unsigned char *)key, ht->size);
 
 	new_node = malloc(sizeof(hash_node_t));
 		if (!new_node)
@@ -26,6 +29,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free(new_node);
 		return (0);
 	}
+	printf("2====================================\n");
 	new_node->key = strcpy(new_node->key, key);
 	new_node->value = strcpy(new_node->value, value);
 
@@ -34,6 +38,13 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	else
 		new_node->next = ht->array[indx];
 
+	printf("who am i? %s\n", key);
+	printf("which val am i? %s\n", value);
+	if (new_node->next)
+		printf("which is my next? %s\n", new_node->next->key);
+	if (ht->array[indx])
+		printf("estas encontrando cabeza? %s\n", ht->array[indx]->key);
+	printf("3====================================\n");
 	ht->array[indx] = new_node;
 	return (1);
 }
