0e0cf2ac79e9ea0522397ccf582000cfc50870aa
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-05-28 10:29:52 -0300

    a

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,26 +1,31 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_delete - deletes a hash table
+ * @ht: the hash table to delete
+ * ---------------------------------
+*/
 void hash_table_delete(hash_table_t *ht)
 {
-    unsigned long int i = 0;
-    hash_node_t *cursor = NULL, *aux = NULL;
+	unsigned long int i = 0;
+	hash_node_t *cursor = NULL, *aux = NULL;
 
-    if (!ht)
-        return;
+	if (!ht)
+		return;
 
-    for (i = 0; i < ht->size; i++)
-    {
-        if (!ht->array[i])
-            continue;
-        
-        cursor = ht->array[i];
-        while (cursor)
-        {
-            aux = cursor->next;
-            free(cursor->key);
-            free(cursor->value);
-            free(cursor);
-            cursor = aux;
-        }
-    }
+	for (i = 0; i < ht->size; i++)
+	{
+		if (!ht->array[i])
+			continue;
+
+		cursor = ht->array[i];
+		while (cursor)
+		{
+			aux = cursor->next;
+			free(cursor->key);
+			free(cursor->value);
+			free(cursor);
+			cursor = aux;
+		}
+	}
 }
