1d7f167d07dddc65bfd8d2c464916872c7ae287b
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-05-28 17:17:33 -0300

    que vida ingrata

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,5 +1,7 @@
 #include "hash_tables.h"
 
+void free_node(hash_node_t *node);
+
 /**
  * hash_table_set - add a new item in the table
  * @ht: the hash table
@@ -15,14 +17,12 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (!ht || !key || strcmp(key, "") == 0 || !value || !ht->array)
 		return (0);
-
 	indx = key_index((const unsigned char *)key, ht->size);
 	new_node = malloc(sizeof(hash_node_t));
 		if (!new_node)
 			return (0);
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
-
 	if (!new_node->key || !new_node->value)
 	{
 		if (!new_node->value)
@@ -32,7 +32,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free(new_node);
 		return (0);
 	}
-
 	if (!ht->array[indx])
 		new_node->next = NULL;
 	else
@@ -40,12 +39,32 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		if (strcmp(new_node->key, ht->array[indx]->key) == 0)
 		{
 			free(ht->array[indx]->value);
-			ht->array[indx]->value = new_node->value;
+			ht->array[indx]->value = strdup(new_node->value);
+			if (!ht->array[indx]->value)
+			{
+				free_node(ht->array[indx]);
+				return (0);
+			}
+			free_node(new_node);
+			return (1);
 		}
 		else
 			new_node->next = ht->array[indx];
 	}
-
 	ht->array[indx] = new_node;
 	return (1);
+}
+
+/**
+ * free_node - frees a node
+ * @node: the node
+ * ------------------------------
+*/
+void free_node(hash_node_t *node)
+{
+	if (node->key)
+		free(node->key);
+	if (node->value)
+		free(node->value);
+	free(node);
 }
