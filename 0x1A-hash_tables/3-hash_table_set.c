2a848b1d900abe7d6fab5c67fab60fc11cfe9238
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-05-28 09:49:09 -0300

    a

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,7 +13,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int indx = 0;
 	hash_node_t *new_node = NULL;
 
-	if (!ht || !key || strcmp(key, "") == 0)
+	if (!ht || !key || strcmp(key, "") == 0 || !value)
 		return (0);
 
 	indx = key_index((const unsigned char *)key, ht->size);
