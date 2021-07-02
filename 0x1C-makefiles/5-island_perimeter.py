b130f3459f4da8723c5c06cf6c7e1e106e9c1298
HernanMontenegro <hernanmontenegro011023@gmail.com>
2021-07-02 18:28:46 +0000

    a

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -9,12 +9,29 @@ def island_perimeter(grid):
         for i in range(1, len(grid[row]) - 1):
             if (grid[row][i] == 0):
                 continue
-            if (grid[row][i - 1] == 0):
+            
+            if (row - 1 < 0):
                 count += 1
-            if (grid[row][i + 1] == 0):
+            else:
+                if (grid[row - 1][i] == 0):
+                    count += 1
+            
+            if (row + 1 >= len(grid)):
                 count += 1
-            if (grid[row - 1][i] == 0):
+            else:
+                if (grid[row + 1][i] == 0):
+                    count += 1
+            
+            if (i - 1 < 0):
                 count += 1
-            if (grid[row + 1][i] == 0):
+            else:
+                if (grid[row][i - 1] == 0):
+                    count += 1
+
+            if (i + 1 >= len(grid[row])):
                 count += 1
+            else:
+                if (grid[row][i + 1] == 0):
+                    count += 1
+
     return count
